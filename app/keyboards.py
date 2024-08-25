8791434ce32d29b08e5c58e411efc20eef093531
Assylzhan <rachmatullinasilzhan@gmail.com>
2024-08-25 13:12:07 +0500

    bug fix in lang keyboard

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-lang = InlineKeyboardMarkup(keyboard=[
+lang = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Русский', callback_data='russian_lang')],
         [InlineKeyboardButton(text='Қазақша', callback_data='kazakh_lang')],
         [InlineKeyboardButton(text='English', callback_data='english_lang')]
